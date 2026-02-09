2631f262b30fd93861ffe52b5e85e64d71a5c3ea
abrandonment <brandonmichael109@gmail.com>
2026-02-09 15:28:56 -0500

    UI updates + Scene Transitions
    -Fixes text errors in UI
    -Adds transitions between scenes, using SceneTransition prefab

---
+++ Rapid_Prototype_1/Assets/SceneLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    //Loads Next Level in build
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play Transition Animation
+        transition.SetTrigger("Start");
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
